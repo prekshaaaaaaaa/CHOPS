75fabbb1dc6405bec8c8f34966bb5017a04113d3
prekshaaaaaaaa <168933731+prekshaaaaaaaa@users.noreply.github.com>
2025-07-11 05:44:43 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import "./header.css";
 
 function Header() {
     return (
-        <div className="container">
-            <img src="/images/home.png" alt="Home" className="header-img" style={{width: '100%', maxHeight: '180px', objectFit: 'cover', borderRadius: '12px', marginBottom: '16px'}} />
+        <div
+            className="container header-bg"
+            style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/home.png)`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}
+        >
             <h3 className="heading animated-heading">CHOPS</h3>
             <p className="subhead animated-heading">Where every bite tells a tale of flavour and passion</p>
-            {/* Navigation removed as requested */}
         </div>
     );
 }
 
 export default Header;
+
